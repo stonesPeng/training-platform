38c9679cc666767487050568435b21357f650ff5
honor_stone <honor_stone@163.com>
2021-06-08 22:34:18 +0800

    commit more

--- training-java/crypto/src/main/java/me/stone/training/platform/training/java/crypto/digest/DigestUtils.java
+++ training-java/crypto/src/main/java/me/stone/training/platform/training/java/crypto/digest/DigestUtils.java
@@ -32,6 +32,14 @@ public interface DigestUtils {
         return digest(bytes, AlgorithmType.SHA1);
     }
 
+    /**
+     * 将待加密的内容和算法名称作为输入，并返回计算出的哈希值的十六进制形式
+     *
+     * @param aByte     待加密的内容
+     * @param algorithm 算法名称
+     * @return 哈希值的十六进制形式
+     * @throws NoSuchAlgorithmException 没有找到加密算法异常
+     */
     static String digest(byte[] aByte, @NotNull String algorithm) throws NoSuchAlgorithmException {
         final MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
         final byte[] digest = messageDigest.digest(aByte);
