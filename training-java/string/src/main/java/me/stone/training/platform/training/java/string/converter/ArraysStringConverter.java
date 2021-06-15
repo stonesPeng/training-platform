3e316b2c2956d549476434eb968911f7889c7c23
honor_stone <honor_stone@163.com>
2021-06-15 22:48:05 +0800

    improve the function of ArraysStringConverter

--- training-java/string/src/main/java/me/stone/training/platform/training/java/string/converter/ArraysStringConverter.java
+++ training-java/string/src/main/java/me/stone/training/platform/training/java/string/converter/ArraysStringConverter.java
@@ -22,4 +22,17 @@ public interface ArraysStringConverter {
         }
         return stringBuffer.toString();
     }
+
+    static byte[] HexStringToByteArray(String s) throws IllegalArgumentException {
+        final int len = s.length();
+        if (len % 2 == 1) {
+            throw new IllegalArgumentException("Hex string must have even number of characters");
+        }
+        final byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
 }
